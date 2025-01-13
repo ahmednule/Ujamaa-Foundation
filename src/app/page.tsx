c6f36ed169f9f1328053b8a1a3224e08aad35d92
ahmednule <nuleahmed6@gmail.com>
2025-01-13 03:59:35 -0800

    feature: created the about component then nested it inside the / so that when something new is added the page will immedialtely reflect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
 "use client";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@components/components/ui/carousel";
+import {Carousel,CarouselContent,CarouselItem,CarouselNext,CarouselPrevious,} from "@components/components/ui/carousel";
 import { Button } from "@components/components/ui/button";
 import Image from "next/image";
 import { items } from "@components/lib/HomeLinks";
+import AboutUs from "@components/components/AboutUs"
 
 export default function Home() {
   return (
@@ -48,6 +43,7 @@ export default function Home() {
         <CarouselPrevious className="absolute top-1/2 transform -translate-y-1/2 left-4 bg-white text-black rounded-full p-3 shadow-lg" />
         <CarouselNext className="absolute top-1/2 transform -translate-y-1/2 right-4 bg-white text-black rounded-full p-3 shadow-lg" />
       </Carousel>
+      <AboutUs/>
     </>
   );
 }
---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,8 @@
+"use client"
+import { Typography } from "@mui/material"
+export default function AboutUs(){
+    return(
+        <Typography variant="h3">This is the about component</Typography>
+
+    )
+}
