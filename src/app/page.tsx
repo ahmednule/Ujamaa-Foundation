57a12c118ec28966210ec63777c32a09acf2d716
ahmednule <nuleahmed6@gmail.com>
2025-01-13 01:04:27 -0800

    feature: added a caurosel so that users can be able to get to know what the wesbite does just from a pip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,47 @@
-"use client"
+"use client";
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@components/components/ui/carousel";
+import { Button } from "@components/components/ui/button";
+import Image from "next/image";
+
 export default function Home() {
-  return(
+  const images = [
+    "/image1.jpg", 
+    "/image2.jpg",
+    "/image3.jpg",
+    "/image4.jpg",
+    "/image5.jpg",
+
+  ];
+
+  return (
     <>
-    <h1>ujamaa Foudation</h1>
+      <Carousel>
+        <CarouselContent>
+          {images.map((src, index) => (
+            <CarouselItem key={index}>
+              
+              <Image
+                src={src}
+                alt={`Carousel image ${index + 1}`}
+                width={300} 
+                height={400}
+                layout="responsive"
+              />
+              
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+        <Button>Custom Action</Button>
+      </Carousel>
     </>
-  )
+  );
 }
