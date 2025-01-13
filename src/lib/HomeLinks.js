ecc9e1e4d85e43c5f9b7be054aaae8c10dd978c5
ahmednule <nuleahmed6@gmail.com>
2025-01-13 03:10:21 -0800

    fix; replaced the statis routes to the ones actually declared so that when a user now clicks on one link, he/she goes to where he/she wanted to

--- src/lib/HomeLinks.js
+++ src/lib/HomeLinks.js
@@ -2,27 +2,27 @@
 export const items = [
     {
       src: "/image1.jpeg",
-      text: "Explore Mountains",
-      link: "/mountains"
+      text: "Who We Are",
+      link: "/who-we-are"
     },
     {
       src: "/image2.jpeg",
-      text: "Discover Beaches",
-      link: "/beaches"
+      text: "Our Programs",
+      link: "/our-programs"
     },
     {
       src: "/image3.jpeg",
-      text: "Visit Cities",
-      link: "/cities"
+      text: "Latest Activities",
+      link: "/latest-activities"
     },
     {
       src: "/image4.jpeg",
-      text: "Experience Forests",
-      link: "/forests"
+      text: "Contact Us",
+      link: "/contact-us"
     },
     {
       src: "/image5.jpeg",
-      text: "Adventure Awaits",
-      link: "/adventures"
+      text: "Home",
+      link: "/"
     },
   ];
