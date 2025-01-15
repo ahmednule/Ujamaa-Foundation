64dd79c63ffd4478d937a418636f002c2971bc47
ahmednule <nuleahmed6@gmail.com>
2025-01-15 04:15:02 -0800

    feature: created the section TargetBeneficiaries then added the h3 within a typograpgy from MUI

---
+++ src/components/ui/TargetBeneficiaries.jsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material";
+
+export default function TargetBeneficiaries(){
+    return (
+        <div>
+            <Typography variant="h3"> OUR TARGET BENEFICIARIES</Typography>
+        </div>
+    )
+}
