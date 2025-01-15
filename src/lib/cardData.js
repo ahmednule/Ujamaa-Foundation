53fd73d11da7f2e4b464e99b9b9c778f18afbf3b
ahmednule <nuleahmed6@gmail.com>
2025-01-15 03:21:45 -0800

    feature: crtteaed the folder cardData that will be used to hold the details of the latest servvices that will later be mapped inside the LatestNews component

---
+++ src/lib/cardData.js
@@ -0,0 +1,16 @@
+export const cardData = [
+    {
+      title: "Tree Planting Day",
+      description: "Ujamaa Foundation has over the years been involved in activities aimed at promoting natural resources management to improve the situation of drought.",
+      expandedDescription: "To this end, Ujamaa Foundation has established tree planting. These trees are planted in degraded areas among the communities as well as public institutions such as schools, hospitals, and administrative areas to improve tree cover. Most of the trees raised are those of local tree species that are capable of surviving in these dry areas.",
+      image: "image2.jpeg",
+    },
+    {
+      title: "Water Conservation Efforts",
+      description: "Ujamaa Foundation is also involved in initiatives focused on water conservation, especially in regions facing severe water scarcity.",
+      expandedDescription: "The foundation has implemented various rainwater harvesting systems and community education programs to help local populations manage their water resources efficiently.",
+      image: "image3.jpeg",
+    },
+  
+  ];
+  
