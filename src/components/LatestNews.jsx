84732a34271bd7bdadc30ff0c61c54df7a471b24
ahmednule <nuleahmed6@gmail.com>
2025-01-15 03:52:42 -0800

    feature: update the cards to be responsive then went ahead and added more details on the work done by Ujamaa Foundation

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -1,6 +1,7 @@
 import { Typography, Card, CardContent, CardMedia, IconButton, Collapse } from "@mui/material";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import React, { useState } from "react";
+import {cardData} from "@components/lib/cardData"
 
 export default function LatestNews() {
   const [expanded, setExpanded] = useState(false);
@@ -11,29 +12,31 @@ export default function LatestNews() {
 
   return (
     <>
-      <Typography variant="h3" color="green" className="text-center text-2xl mt-3">LATEST NEWS</Typography>
-      <Card sx={{ maxWidth: 500, borderRadius: "16px", marginLeft: "30px"}} className="hover:scale-105">
-        <CardMedia
-          component="img"
-          alt="Ujamaa Foundation Initiatives"
-          height="200"
-          image="image2.jpeg"
-        />
-        <CardContent>
-          <Typography variant="h5" color="green">Tree Planting Day</Typography>
+      <Typography variant="h3" color="green" className="text-center text-2xl mt-4">LATEST NEWS</Typography>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      {cardData.map((card, index) => (
+      <Card key={index} sx={{ maxWidth: 500, borderRadius: "16px", marginLeft: "30px"}} className="hover:scale-105">
+             <CardMedia
+             component="img"
+             alt={card.title}
+             height="200"
+             image={card.image}
+           /> 
+            <CardContent>
+          <Typography variant="h5" color="green">{card.title}</Typography>
           <Typography variant="body1">
-            Ujamaa Foundation has over the years been involved in activities aimed at promoting natural resources management to improve the situation of drought. 
+            {card.description}
           </Typography>
         </CardContent>
         <CardContent>
           <Collapse in={expanded} timeout="auto" unmountOnExit>
             <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-            To this end, Ujamaa Foundation has established tree planting. These trees are planted in degraded areas among the communities as well as public institutions such as schools, hospitals, and administrative areas to improve tree cover. Most of the trees raised are those of local tree species that are capable of surviving in these dry areas.
+                {card.expandedDescription}
             </Typography>
           </Collapse>
           <IconButton
-            onClick={handleExpandClick}
-            aria-expanded={expanded}
+            onClick={() => handleExpandClick(index)}
+            aria-expanded={expanded === index}
             aria-label="show more"
             sx={{ marginLeft: "auto", display: "block" }}
           >
@@ -41,6 +44,8 @@ export default function LatestNews() {
           </IconButton>
         </CardContent>
       </Card>
+        ))}
+       </div>
     </>
   );
 }
--- src/lib/cardData.js
+++ src/lib/cardData.js
@@ -11,6 +11,20 @@ export const cardData = [
       expandedDescription: "The foundation has implemented various rainwater harvesting systems and community education programs to help local populations manage their water resources efficiently.",
       image: "image3.jpeg",
     },
+    {
+        title: "Training and Capacity Building",
+        description: "Giving back to the community is the key role for Ujamaa Foundation.",
+        expandedDescription: "Ujamaa Foundation has been forefront training the community and doing engagement that has brought a every informative youths and Women across theCounty. Financial services like financial literacy engagement was done and the locals and non-locals got the opportunity to be first 40 graduates under the watch of Ujamaa Foundation.",
+        image: "image7.jpg",
+      },
+      {
+        title: "Education Sector Support",
+        description: "Access to quality and affordable education the right of every child in Kenya.",
+        expandedDescription: "For a pastoralist child, this has never been an easy thing. Pastoralists are often split between sending their children to school or getting involved in finding means of survival through herding animals for boys and looking for water and other household chores for girls. Literacy levels in theseareas are very low and school dropout levels are high as children drop out of school when natural disasters like droughts occur.",
+        image: "image9.png",
+      },
+      
+    
   
   ];
   
