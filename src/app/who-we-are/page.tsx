76b0e3e885860b005a35282216830e8cb9d78008
ahmednule <nuleahmed6@gmail.com>
2025-01-16 02:47:51 -0800

    feature: removed the commented import inside the /

--- src/app/who-we-are/page.tsx
+++ src/app/who-we-are/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 import AboutusCarosel from "@components/components/ui/AboutusCarosel"
-// import { Typography } from "@mui/material"
 export default function whoWeare(){
     return(
         <AboutusCarosel/>
