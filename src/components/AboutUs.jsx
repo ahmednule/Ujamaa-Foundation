f9614596f7d94ebd20dbd84dd220f1478fa9b55f
ahmednule <nuleahmed6@gmail.com>
2025-01-16 02:05:39 -0800

    update: just updated the mission of the foundation so that it appears on the top of the component representing the diffrence

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -6,19 +6,21 @@ export default function AboutUs() {
   return (
     <div className="p-8 flex flex-col md:flex-row items-center gap-8">
       <div className="flex-1">
-        <Typography
+       <div className="relative h-40 ">
+       <Typography
           variant="h5"
-          className="text-green-500 font-bold mb-4"
+          className="text-green-500 font-bold mb-1 absolute left-0 top-0 h-25 w-30"
         >
-          Ujamaa Foundation, Sowing The Future.aas
+          Ujamaa Foundation, Sowing The Future 
         </Typography>
+       </div>
         <Typography
-          variant="h3"
-          className="text-[#ADD8E6] font-bold mb-4"
+          variant="h4"
+          className="text-[#ADD8E6] font-bold mb-7"
         >
           ABOUT US
         </Typography>
-        <Typography variant="body1" className="leading-relaxed">
+        <Typography variant="body1" className="leading-relaxed text-balance mt-5">
           Ujamaa Foundation is a non-profit, community-based, and youth-led organization
           registered in 2022 under the Department of Social Protection in Wajir County,
           Kenya. The organization is committed to empowering communities, particularly in
