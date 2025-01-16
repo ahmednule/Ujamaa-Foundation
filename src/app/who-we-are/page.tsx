d1ef7475d426c1750df5bfad9984ec0fd82ee497
ahmednule <nuleahmed6@gmail.com>
2025-01-16 02:52:19 -0800

    fetaure: inside the who we are page, nested the About us component since the component details are not changing, will update the header vto look more better later

--- src/app/who-we-are/page.tsx
+++ src/app/who-we-are/page.tsx
@@ -1,7 +1,11 @@
 "use client"
+import AboutUs from "@components/components/AboutUs"
 import AboutusCarosel from "@components/components/ui/AboutusCarosel"
 export default function whoWeare(){
     return(
+        <>
         <AboutusCarosel/>
+        <AboutUs/>
+        </>
     )
 }
