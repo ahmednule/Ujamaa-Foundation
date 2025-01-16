88b006d95f08bd17b4255f012867664ae2f18e39
ahmednule <nuleahmed6@gmail.com>
2025-01-15 23:37:52 -0800

    feature: inside the target beneficiary added bthe image which shows what ujamaa foundation is all about then went ahead and created a button which when clicked will be taking a user to the contact us page which users can donate to helppush the organization further

--- src/components/ui/TargetBeneficiaries.jsx
+++ src/components/ui/TargetBeneficiaries.jsx
@@ -1,9 +1,39 @@
+"use client"
 import { Typography } from "@mui/material";
+import {Card, CardHeader, CardContent, Button} from "@mui/material";
+import { CardTitle } from "./card";
 
-export default function TargetBeneficiaries(){
+export default function TargetBeneficiaries() {
     return (
-        <div>
-            <Typography variant="h3"> OUR TARGET BENEFICIARIES</Typography>
+        <div
+            className="relative bg-cover bg-center h-[400px] flex items-center justify-center text-white mt-11"
+            style={{
+                backgroundImage: "url('/image12.jpg')",
+            }}
+        >
+            <Card className="ml-3">
+                <CardTitle>
+                <Typography
+                variant="h3"
+                className="text-left font-bold text-2xl md:text-3xl lg:text-4xl drop-shadow-lg ml-3"
+            >
+                <span className="text-green-400 ml-4">OUR TARGET </span>   <span className="text-green-900">  BENEFICIARIES</span>
+            </Typography>
+                </CardTitle>
+                <CardContent>
+                    <Typography variant="body1" className="text-balance" style={{maxWidth:"700px"}}> 
+                    Ujamaa Foundation is a not for profit, Community based, non governmental Youth led
+organization registered in the department of social development, Wajir County in 2022.
+Ujamaa Foundation works with established youth groups and women groups in the six sub
+counties in empowering the community on development issues, Quality Education, Health,
+environmental protection and climate change mitigation and Peace justice and strong institutions.
+                    </Typography>
+                    <div className="flex flex-row-reverse mb-7 relative">
+                    <Button variant="contained" size="large">SUPPORT US</Button>
+                    </div>
+                   
+                </CardContent>
+                </Card>
         </div>
-    )
+    );
 }
