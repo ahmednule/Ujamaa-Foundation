7345db3983c3057f43e63b3d9ff6a8d18ec5be43
ahmednule <nuleahmed6@gmail.com>
2025-01-16 02:27:16 -0800

    feature: created the avoutuscarosel which will be put inside the who we are page talinkg about what ujamaa foundation is all about

---
+++ src/components/ui/AboutusCarosel.jsx
@@ -0,0 +1,11 @@
+"use client"
+import { Typography } from "@mui/material"
+export default function (){
+    return (
+        <div classsName="relative bg-cover bg-center h-[400px flex flex-items justify-center text-white"
+        style={{backgroundImage:"url(image6.jpeg)"}}
+        >
+            <Typography variant="h4">About Us</Typography>
+        </div>
+    )
+}
