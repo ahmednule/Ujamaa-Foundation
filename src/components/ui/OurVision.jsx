39feebdff94929eb7f74862772ddf0c0cabec100
ahmednule <nuleahmed6@gmail.com>
2025-01-16 03:04:12 -0800

    feature: cretaed the vision card as a ui that will combine the rest to one

---
+++ src/components/ui/OurVision.jsx
@@ -0,0 +1,18 @@
+import { Card, Typography, CardContent } from "@mui/material"
+export default function OurVision(){
+    return( 
+            <div>
+            <Card>
+                <CardContent>
+                    <Typography variant="h3">
+                        OUR VISION
+                    </Typography>
+                    <Typography variant="body1">
+                        To Promote SDG awareness and provide a platform for youth and women to actively participate in 
+                        addressing economic challenges affecting thie communities inclusively and sustnainably.
+                    </Typography>
+                </CardContent>
+            </Card>
+            </div>
+    )
+}
