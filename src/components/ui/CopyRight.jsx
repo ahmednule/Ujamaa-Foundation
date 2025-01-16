8e5646cb649c7e9309a06e049dcae22f976939e0
ahmednule <nuleahmed6@gmail.com>
2025-01-16 01:19:48 -0800

    feature: cretaed a componenet called vo[yright which will be put in the footer section showing how who designed the wesite

---
+++ src/components/ui/CopyRight.jsx
@@ -0,0 +1,9 @@
+"use client"
+import { Typography } from "@mui/material"
+export default function CopyRight(){
+    return(
+        <div>
+            <Typography variant="h5"> Copyright ©2025 | UF | website developed by Goymarey 9th Unga House Westlands Kenya</Typography>
+        </div>
+    )
+}
