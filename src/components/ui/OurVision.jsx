2cd67c3fce2f3694c5254e6c3c4a0705db151cab
ahmednule <nuleahmed6@gmail.com>
2025-01-16 06:02:40 -0800

    fetaure: cretaed the vision card, instad of using mui just sticked to tailwind css

--- src/components/ui/OurVision.jsx
+++ src/components/ui/OurVision.jsx
@@ -1,19 +1,23 @@
 "use client"
-import { Card, Typography, CardContent } from "@mui/material"
+import { Typography } from "@mui/material"
 export default function OurVision(){
     return( 
-            <div className="max-w-">
-            <Card>
-                <CardContent>
-                    <Typography variant="h3">
-                        OUR VISION
-                    </Typography>
-                    <Typography variant="body1">
-                        To Promote SDG awareness and provide a platform for youth and women to actively participate in 
-                        addressing economic challenges affecting thie communities inclusively and sustnainably.
-                    </Typography>
-                </CardContent>
-            </Card>
+            <div
+            className="max-w-96 m-6 bg-white border border-green-300 p-6 rounded-lg shadow-lg transform transition duration-300 hover:scale-105 animate-grow"
+          >
+             <Typography
+              variant="h6"
+              className="text-orange-600 font-bold mb-4 text-center"
+            >
+             OUR VISION
+            </Typography>
+            <Typography
+              variant="body2"
+              className="text-gray-700 leading-relaxed mb-6 text-center text-balance"
+            >
+              To Promote SDG awareness and provide a platform for youth and women to actively participate in 
+              addressing economic challenges affecting thie communities inclusively and sustnainably.
+            </Typography>
             </div>
     )
 }
