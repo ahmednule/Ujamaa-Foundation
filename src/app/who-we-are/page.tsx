9b0a939bba1c15adaf30612dc454d1e097e81692
ahmednule <nuleahmed6@gmail.com>
2025-01-16 03:06:28 -0800

    fetaure: nested the vision card inside the who we are page inside a div that i willl later use to make the other cards to be responsive

--- src/app/who-we-are/page.tsx
+++ src/app/who-we-are/page.tsx
@@ -1,11 +1,15 @@
 "use client"
 import AboutUs from "@components/components/AboutUs"
 import AboutusCarosel from "@components/components/ui/AboutusCarosel"
+import OurVision from "@components/components/ui/OurVision"
 export default function whoWeare(){
     return(
         <>
         <AboutusCarosel/>
         <AboutUs/>
+        <div>
+            <OurVision/>
+        </div>
         </>
     )
 }
