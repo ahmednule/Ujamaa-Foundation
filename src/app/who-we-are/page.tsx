09ca404386254685a3b37e4c9a7ff8b773f15d2b
ahmednule <nuleahmed6@gmail.com>
2025-01-16 06:16:27 -0800

    fetaure: inside the whoWeare component where the cards are netesed implemented them so that on a smal;l sceen they can be 1 on a md:2 and large3"

--- src/app/who-we-are/page.tsx
+++ src/app/who-we-are/page.tsx
@@ -8,7 +8,7 @@ export default function whoWeare(){
         <>
         <AboutusCarosel/>
         <AboutUs/>
-        <div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
             <OurVision/>
             <OurMission/>
         </div>
