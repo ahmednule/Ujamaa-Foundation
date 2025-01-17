5b77eae3dc3e857e5e86806cac7cfbc8c3393466
ahmednule <nuleahmed6@gmail.com>
2025-01-17 03:59:45 -0800

    fetaure: for the latest news, nested the cards that exists insdide the latest activities page

--- src/app/latest-activities/page.tsx
+++ src/app/latest-activities/page.tsx
@@ -1,9 +1,14 @@
 "use client"
 import LatestactivitiesCarosel from "@components/components/ui/LatestactivitiesCarosel"
+import LatestNews from "@components/components/LatestNews"
+
 export default function latestActivities(){
     return (
       <div>
         <LatestactivitiesCarosel/>
+       <div className="mt-5">
+       <LatestNews/>
+       </div>
       </div>
     )
 }
