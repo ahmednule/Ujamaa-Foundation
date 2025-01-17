61821e9ad2d9cbe4cab76c2ba1f4eb9c2cb37514
ahmednule <nuleahmed6@gmail.com>
2025-01-17 03:42:22 -0800

    fetaur: just styled the core values better for consistency

--- src/components/OurcoreValues.jsx
+++ src/components/OurcoreValues.jsx
@@ -5,7 +5,7 @@ export default function OurcoreValues() {
     return (
         <div className="p-8">
             <Typography variant="h4" className="text-center m-11">
-                OUR CORE VALUES
+              <span className="text-green-600">OUR CORE  </span>  VALUES
             </Typography>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 md:grid-cols-4 gap-6">
                 {OurcoreValuesData.map((item, index) => (
