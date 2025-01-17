2fabac7b9368bbad0634f8b7aaee0ad18a257a28
ahmednule <nuleahmed6@gmail.com>
2025-01-16 23:20:43 -0800

    fetaure: created the component which will be used to hold the core values of the foundation

---
+++ src/components/OurcoreValues.jsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material";
+
+export default function OurcoreValues(){
+    return(
+        <div>
+            <Typography variant="h4">OUR CORE VALUES</Typography>
+        </div>
+    )
+}
