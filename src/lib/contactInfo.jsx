aa0c04bda33b24e3b643e4d16bd4531560ea0af2
ahmednule <nuleahmed6@gmail.com>
2025-01-17 02:00:37 -0800

    refactor2

---
+++ src/lib/contactInfo.jsx
@@ -0,0 +1,6 @@
+const contactInfo = [
+    { icon: <Phone />, text: "+254 722527325", label: "CEO Ujamaa Foundation" },
+    { icon: <Email />, text: "iabdirahman030@gmail.com", label: "CEO Email" },
+    { icon: <Phone />, text: "+254 722413082", label: "Finance Manager" },
+    { icon: <Email />, text: "abdullahialim2@gmail.com", label: "Finance Email" }
+]
