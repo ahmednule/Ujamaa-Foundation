91277523b6ca9d3b2b0a1dbde3ee5d108f60118a
ahmednule <nuleahmed6@gmail.com>
2025-01-16 23:38:34 -0800

    update: added use client inside the outcorevalues cards

--- src/components/OurcoreValues.jsx
+++ src/components/OurcoreValues.jsx
@@ -1,9 +1,11 @@
 import { Typography } from "@mui/material";
-
+import {OurcoreValuesData} from "@components/lib/OurcoreValuesData"
 export default function OurcoreValues(){
     return(
-        <div>
+        <div className="max-w-96 ">
             <Typography variant="h4">OUR CORE VALUES</Typography>
+
+
         </div>
     )
 }
--- src/lib/OurcoreValuesData.js
+++ src/lib/OurcoreValuesData.js
@@ -1,3 +1,4 @@
+"use client"
 export default OurcoreValuesData = [
     {
         value: "Integrity",
