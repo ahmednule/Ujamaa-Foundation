ac8ff74f7a9d16d8a44010599d1ad95fa4c87ace
ahmednule <nuleahmed6@gmail.com>
2025-01-17 05:40:02 -0800

    feature: imported the cards inside the main component then went ahead and made it responsive so that in a small screen it will be just one in a big, 2 per sceen

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,11 +1,15 @@
 "use client" 
 import ContactusCarosel from "@components/components/ui/ContactusCarosel"
 import HowtoSupport from "@components/components/ui/HowtoSupport"
+import ContactusForm from "@components/components/ui/ContactusForm"
 export default function contactUs(){
     return(
         <div>
             <ContactusCarosel/>
-            <HowtoSupport/>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
+        <HowtoSupport/>
+        <ContactusForm/>
+        </div>
         </div>
     )
 }
