431d6831f236843b06772f86330639b923a3ec0f
ahmednule <nuleahmed6@gmail.com>
2025-01-16 23:34:12 -0800

    fetaure: cretead the data for the our core values cards inside another file thne next ill map them inside the main file so that maintaining the code would be easy

---
+++ src/lib/OurcoreValuesData.js
@@ -0,0 +1,18 @@
+export default OurcoreValuesData = [
+    {
+        value: "Integrity",
+        description: "We uphold the highest standards of honesty and transparency in all our actions, ensuring accountability to our beneficiaries, donors, and partners."
+    },
+    {
+        value: "Accountability",
+        description: "We take full responsibility for our actions and decisions, remaining answerable to those we serve and those who support our mission, fostering trust and reliability."
+    },
+    {
+        value: "Professionalism",
+        description: "We maintain the utmost dedication, competence, and ethical conduct in all our efforts, delivering services with excellence and care to empower those in need."
+    },
+    {
+        value: "Respect for Diversity",
+        description: "We embrace and celebrate the unique backgrounds, cultures, and perspectives of every individual, ensuring inclusivity and fairness in all aspects of our work."
+    }
+];
