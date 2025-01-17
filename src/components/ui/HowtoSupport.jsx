0df4a8e456a03e6322f6101e0dbd8829b895dadf
ahmednule <nuleahmed6@gmail.com>
2025-01-17 05:08:36 -0800

    added icons fo the support data

--- src/components/ui/HowtoSupport.jsx
+++ src/components/ui/HowtoSupport.jsx
@@ -1,9 +1,13 @@
 "use client"
-import { Typography } from "@mui/material"
+// import { Typography } from "@mui/material"
 export default function HowtoSupport(){
     return (
         <div>
-            <Typography variant="h4">How Can you support</Typography>
+           <h2 className="m-8 text-5xl font-bold">How You Can support</h2>
+           <div className="max-w-96 border-green-500 rounded-lg shadow p-6 ">
+            <h2 className="text-4xl mb-3">Donate</h2>
+            <p>Your contribution help us provide essentiaal services and empower the communities we serve.</p>
+           </div>
         </div>
     )
 }
---
+++ src/lib/HowtoSupportData.js
@@ -0,0 +1,19 @@
+import VolunteerActivismIcon from '@mui/icons-material/VolunteerActivism';
+import JoinFullIcon from '@mui/icons-material/JoinFull';
+import GroupIcon from '@mui/icons-material/Group';
+export const HowtoSupportData=[
+    {
+        value:"Donate",
+        icon:<VolunteerActivismIcon/>,
+        description:"Your contribution help us provide essentiaal services and empower the communities we serve."
+    },
+    {
+        value:"Volunter",
+        icon:<JoinFullIcon/>,
+        description:"Join our team and make tangible diffrence by offering you're time and skills to make a diffrence."
+    },{
+        value:"Patner",
+        icon:<GroupIcon/>,
+        description:"Collaborate with us and make a long lasting impact to the communities in need "
+    }
+]
