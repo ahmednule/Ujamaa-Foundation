da8905494a2f22e22d03aef4f3bbbd54c8e3995b
ahmednule <nuleahmed6@gmail.com>
2025-01-17 04:12:14 -0800

    feature: created the ContactusCarosel which will be insid ethe contact/donate us page

---
+++ src/components/ui/ContactusCarosel.jsx
@@ -0,0 +1,13 @@
+export default function ContactusCarosel(){
+    return(
+        <div
+        className="relative bg-cover bg-center h-[400px] flex items-center justify-center text-white"
+        style={{
+            backgroundImage: "url('/image6.jpeg')",
+            filter:"brightness(.5)"
+        }}
+    >
+            <Typography variant="h2">Contact Us</Typography>
+        </div>
+    )
+}
