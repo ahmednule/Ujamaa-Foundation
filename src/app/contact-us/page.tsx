8941e7380d1b9057f24f502da1c92d4476cc9e79
ahmednule <nuleahmed6@gmail.com>
2025-01-17 04:48:00 -0800

    feature: cretaed the How to suppport component then went ahead and nested it inside the contactuspage

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,9 +1,11 @@
 "use client" 
 import ContactusCarosel from "@components/components/ui/ContactusCarosel"
+import HowtoSupport from "@components/components/ui/HowtoSupport"
 export default function contactUs(){
     return(
         <div>
             <ContactusCarosel/>
+            <HowtoSupport/>
         </div>
     )
 }
---
+++ src/components/ui/HowtoSupport.jsx
@@ -0,0 +1,9 @@
+"use client"
+import { Typography } from "@mui/material"
+export default function HowtoSupport(){
+    return (
+        <div>
+            <Typography variant="h4">How Can you support</Typography>
+        </div>
+    )
+}
