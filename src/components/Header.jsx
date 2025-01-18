8a593a9048e4857b34de64f2acc0ac38d0cd35ea
ahmednule <nuleahmed6@gmail.com>
2025-01-17 23:53:12 -0800

    fix: updated the header so that when a user is scroling through the website the header moves up and when he starts to sroll up back again it shows itself

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 "use client"
-import React from 'react';
+
+import React, { useState, useEffect } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Container from '@mui/material/Container';
@@ -7,8 +8,35 @@ import Logo from './Logo';
 import NavigationMenu from './Menu';
 
 const Header = () => {
+  const [visible, setVisible] = useState(true);
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.scrollY;
+      setVisible(
+        (prevScrollPos > currentScrollPos) || 
+        currentScrollPos < 10 
+      );
+      
+      setPrevScrollPos(currentScrollPos);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [prevScrollPos]);
+
   return (
-    <AppBar position="sticky" sx={{backgroundColor:"#e6e6e6",top:0, zIndex:1200}}>
+    <AppBar 
+      position="sticky" 
+      sx={{
+        backgroundColor: "#e6e6e6",
+        top: 0,
+        zIndex: 1200,
+        transform: `translateY(${visible ? 0 : -150}px)`,
+        transition: 'transform 0.3s ease-in-out',
+      }}
+    >
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <div style={{ display: 'flex', alignItems: 'center', flexGrow: 1 }}>
