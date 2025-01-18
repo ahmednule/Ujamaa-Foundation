744714c856b252665e2e0d8ec12ad4d5fb572ddf
ahmednule <nuleahmed6@gmail.com>
2025-01-18 00:41:46 -0800

    feature: education empowerement page added

---
+++ src/app/our-programs/education-empowerment/page.tsx
@@ -0,0 +1,9 @@
+"use client"
+import { Typography } from "@mui/material"
+export default function educationEmpowerement(){
+    return(
+        <div>
+            <Typography variant="h3">This is the Education Empowerement Page</Typography>
+        </div>
+    )
+}
