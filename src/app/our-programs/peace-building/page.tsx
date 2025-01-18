c3e46b43a9c2e5122e9fd4326350d163b1626f3f
ahmednule <nuleahmed6@gmail.com>
2025-01-18 00:39:47 -0800

    feature: added the peace building nested page

---
+++ src/app/our-programs/peace-building/page.tsx
@@ -0,0 +1,9 @@
+"use client"
+import { Typography } from "@mui/material"
+export default function peaceBuilding(){
+    return(
+        <div>
+            <Typography variant="h3">This is the Peace Building Page</Typography>
+        </div>
+    )
+}
