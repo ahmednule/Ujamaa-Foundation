7e3746ba65d5a1f3d525dfe954390a484996278e
ahmednule <nuleahmed6@gmail.com>
2025-01-18 00:46:51 -0800

    fetaure: added the last nested page

---
+++ src/app/our-programs/training-capacity/page.tsx
@@ -0,0 +1,9 @@
+"use client"
+import { Typography } from "@mui/material"
+export default function trainingCapacity(){
+    return(
+        <div>
+            <Typography variant="h3">This is the Training and Capacity Building Page</Typography>
+        </div>
+    )
+}
