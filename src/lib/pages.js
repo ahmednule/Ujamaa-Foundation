7d29d2dcfb5c5bfd51bed5d037f012ad98593e36
ahmednule <nuleahmed6@gmail.com>
2025-01-18 00:02:09 -0800

    feature: inside the pages where the other key programs will be shown, added the subpages for the pages

--- src/lib/pages.js
+++ src/lib/pages.js
@@ -1,9 +1,32 @@
 const pages = [
-    { label: 'HOME', path: '/' },
-    { label: 'WHO ARE WE', path: '/who-we-are' },
-    { label: 'OUR PROGRAMS', path: '/our-programs' },
-    { label: 'LATEST ACTIVITIES', path: '/latest-activities' },
-    { label: 'CONTACT US', path: '/contact-us' },
-  ];
+  { label: 'HOME', path: '/' },
+  { label: 'WHO ARE WE', path: '/who-we-are' },
+  { 
+    label: 'OUR PROGRAMS', 
+    path: '/our-programs',
+    subMenu: [
+      {
+        label: 'Peace Building And Community cohestion',
+        path: '/our-programs/peace-building-and-community-cohestion'
+      },
+      {
+        label: 'Education And Empowerment',
+        path: '/our-programs/education-and-empowerment'
+      },
+      {
+        label: 'Environment Restoration',
+        path: '/our-programs/environment-restoration',
+        fullName: 'Environment Restoration and Natural Resource Management'
+      },
+      {
+        label: 'Training And Capacity',
+        path: '/our-programs/training-and-capacity',
+        fullName: 'Training and Capacity Building'
+      }
+    ]
+  },
+  { label: 'LATEST ACTIVITIES', path: '/latest-activities' },
+  { label: 'CONTACT US', path: '/contact-us' },
+];
 
-  export default pages;
+export default pages;
