f44d8a94df8c9864205ae965672007afe6c569ae
ahmednule <nuleahmed6@gmail.com>
2025-01-20 18:34:11 +0300

    feature: added the linkedin link and icon from mui

--- src/lib/socialLinks.js
+++ src/lib/socialLinks.js
@@ -1,6 +1,8 @@
-import {  Facebook, X, Instagram } from "@mui/icons-material"
+import {  Facebook, X, Instagram,LinkedInIcon  } from "@mui/icons-material"
+
  export const socialLinks = [
     { icon: <Facebook />, url: "https://www.facebook.com/profile.php?id=100069361610849&mibextid=ZbWKwL", label: "Facebook" },
     { icon: <X />, url: "https://x.com/UjamaaFoundtion?t=Tip1_p69pLxAkDSnW7kGqA&s=09", label: "X" },
-    { icon: <Instagram />, url: "https://www.instagram.com/ujamaa_foundation06", label: "Instagram" }
+    { icon: <Instagram />, url: "https://www.instagram.com/ujamaa_foundation06", label: "Instagram" },
+    {icon: <LinkedInIcon/>, url:"https://www.linkedin.com/in/ujamaa-foundation-816231348?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app", label:"Linkedin"}
 ]
