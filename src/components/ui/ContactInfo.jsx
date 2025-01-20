c5083f8976ee167e51c76c6336a9dc12613eb841
ahmednule <nuleahmed6@gmail.com>
2025-01-20 15:13:54 +0300

    feature: created a contactinfo componenet that will be used to give the details of the clients

---
+++ src/components/ui/ContactInfo.jsx
@@ -0,0 +1,11 @@
+"use client"
+export default function ContactInfo(){
+    return(
+        <div>
+            <div>
+                <h1>contact us on  +254 723025577</h1>
+                <p>email: ujamaafoundation6@gmail.com</p>
+            </div>
+        </div>
+    )
+}
