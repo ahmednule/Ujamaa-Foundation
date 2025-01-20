974580ee1686e7a06dc1f61df439451ea9f9ce7c
ahmednule <nuleahmed6@gmail.com>
2025-01-20 16:43:56 +0300

    fix: using the organization name, replaced the link to IG

--- src/lib/socialLinks.js
+++ src/lib/socialLinks.js
@@ -2,5 +2,5 @@ import {  Facebook, X, Instagram } from "@mui/icons-material"
  export const socialLinks = [
     { icon: <Facebook />, url: "https://www.facebook.com/profile.php?id=100069361610849&mibextid=ZbWKwL", label: "Facebook" },
     { icon: <X />, url: "https://x.com/UjamaaFoundtion?t=Tip1_p69pLxAkDSnW7kGqA&s=09", label: "X" },
-    { icon: <Instagram />, url: "https://www.instagram.com/invites/contact/?utm_source=ig_contact_invite&utm_medium=copy_link&utm_content=x3o0c15", label: "Instagram" }
+    { icon: <Instagram />, url: "https://www.instagram.com/ujamaa_foundation06", label: "Instagram" }
 ]
